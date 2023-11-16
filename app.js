64106ecf988aa1a93db7783a53d0c25b4dc47140
yfy668 <cathy.yf.yang@gmail.com>
2023-11-17 09:25:19 +1000

    update

--- app.js
+++ app.js
@@ -4,3 +4,38 @@ alert(greatText);
 greatText = "Hi, I'm mike again";
 alert(greatText);
 alert(age);
+
+// Get references to HTML elements
+const postcodeInput = document.getElementById('postcodeInput');
+const searchButton = document.getElementById('searchButton');
+const mapDiv = document.getElementById('map');
+const resultsDiv = document.getElementById('results');
+
+// Function to fetch and parse the recycling bin locations
+async function fetchRecyclingBinLocations() {
+  try {
+    const response = await fetch('https://recyclingnearyou.com.au/clothing/');
+    const html = await response.text();
+
+    // Parse the HTML (You may use a library like Cheerio for this)
+    // Extract the recycling bin location data from the HTML
+    // Example: const locations = parseAndExtractLocations(html);
+
+    // Calculate distances, find the nearest bin, and display the results
+    // Example: const nearestBin = findNearestBin(locations, userPostcode);
+
+    // Display the nearest bin on the map and in the resultsDiv
+    // Example: displayOnMapAndResults(nearestBin);
+  } catch (error) {
+    console.error('Error fetching or parsing data:', error);
+  }
+}
+
+// Event listener for the search button
+searchButton.addEventListener('click', () => {
+  const userPostcode = postcodeInput.value.trim();
+  if (userPostcode) {
+    fetchRecyclingBinLocations();
+  }
+});
+
